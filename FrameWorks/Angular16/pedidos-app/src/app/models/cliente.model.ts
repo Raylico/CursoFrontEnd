37a2811e63524c7fea19a3f8306635e3cff601e4
Raylico <rayssajuliodesouza45@gmail.com>
2025-05-27 11:16:16 -0300

    27.05.2025

---
+++ FrameWorks/Angular16/pedidos-app/src/app/models/cliente.model.ts
@@ -0,0 +1,33 @@
+//classe model para manipulação de dados
+
+export class Cliente{
+//   //atributos
+//   private id: number; //tipagem
+//   public nome: string;
+
+//   constructor(id:number, nome:string){
+//     this.id = id;
+//     this.nome = nome;
+//   }
+
+//   //método acesso publico
+//   // getters and setter
+//   public getId():number {
+//     return this.id;
+//   }
+
+//   public setId(id: number):void{
+//     this.id = id;
+//   }
+
+
+//abreviada de escrever o model
+constructor(
+    public id: number,
+    public nome: string
+    ){}
+}
+
+// reflexo do meu diagrama de classes
+//model  
+
